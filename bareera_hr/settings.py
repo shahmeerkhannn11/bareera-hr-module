6c8f60e2916e8aaa3e340878880447bd730901c6
Shahmeer Khan <shahzemaamir@gmail.com>
2025-11-14 19:29:32 +0500

    Prepare for deployment: update settings, add requirements, .gitignore

--- bareera_hr/settings.py
+++ bareera_hr/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-CHANGE_THIS_TO_YOUR_SECRET_KEY'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
